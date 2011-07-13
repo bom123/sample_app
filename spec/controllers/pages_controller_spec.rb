191dee23c85ec8329de43bdf60808fa827923b4a
Sergey Kim <kim.sergey@faktora.net>
2011-07-13 12:37:06 +0300

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -26,7 +26,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title", :content => @base_title + "Contact")
-    end    
+    end   
   end
 
   describe "GET 'about'" do
